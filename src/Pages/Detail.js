27946545b81326d8de48116d51f5a2128dae1b70
Caroline Davis <caroline_davis@live.com>
2021-01-21 16:57:22 +1100

    feat: Add loading and use effect to details page

--- src/Pages/Detail.js
+++ src/Pages/Detail.js
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import { getPokemonApi } from '../Api/index'
 import { useParams } from "react-router-dom";
+import pokeball from '../Assets/pokeball.png'
+import { errorMsg } from '../Utils/Constants'
 
 
 function Detail() {
 
-    const [pokemonDetails, setPokemonDetails] = useState("")
-    const [loading, setLoading] = useState(false)
-    const [loadingError, setLoadingError] = useState(false)
-
     let { pokemon } = useParams()
 
-    // useEffect here to fetch
-    const pokemonInfo = (getPokemonApi(pokemon))
+    const [pokemonDetails, setPokemonDetails] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [loadingError, setLoadingError] = useState(false)
 
+    useEffect(() => {
+        async function pokemonInfo() {
+            setLoading(true)
+            try {
+                setPokemonDetails(await getPokemonApi(pokemon))
+                setLoading(false)
+                setLoadingError(false)
+            } catch {
+                setLoading(false)
+                setLoadingError(true)
+            }
+        }
+        pokemonInfo();
+    }, [])
+
+
+    if (!loading) {
+        console.log(pokemonDetails.sprites.front_default)
+        console.log(pokemonDetails.name)
+        console.log(pokemonDetails.abilities.map(ability => ability.ability.name))
+        console.log(pokemonDetails)
+        console.log(pokemonDetails.held_items.map(heldItem => heldItem.item.name))
+    }
 
     return (
-        <div></div>
+        <>
+            {loading && <img className="spinner" src={pokeball} alt="loading"></img>}
+            {loadingError && <div className="error-msg">{errorMsg}</div>}
+        </>
     )
 
 
